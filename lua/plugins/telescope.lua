d1592037edead598c78a88df770fffcc6e88e1ab
Joseph Schoblaska <joseph@empiricalsecurity.com>
2026-03-11 13:26:32 -0500

    add shortcut for inserting note link

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,64 @@ return {
     local telescope = require("telescope")
 
     local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+
+    local function insert_note_link(prompt_bufnr)
+      local entry = action_state.get_selected_entry()
+      actions.close(prompt_bufnr)
+      if not entry or not entry.filename then return end
+
+      local title
+      if type(entry.value) == "table" and entry.value.title then
+        title = entry.value.title
+      else
+        local f = io.open(entry.filename, "r")
+        if f then
+          local line = f:read("*l")
+          f:close()
+          title = line and line:match("^#%s+(.+)$")
+        end
+        title = title or vim.fn.fnamemodify(entry.filename, ":t:r")
+      end
+
+      -- Normalize both paths relative to notebook root
+      local notebook = vim.env.ZK_NOTEBOOK_DIR or vim.fn.getcwd()
+      local cur_abs = vim.api.nvim_buf_get_name(0)
+      local cur_rel = cur_abs:sub(1, #notebook + 1) == notebook .. "/"
+        and cur_abs:sub(#notebook + 2) or cur_abs
+      local tgt_rel = entry.filename
+      if tgt_rel:sub(1, #notebook + 1) == notebook .. "/" then
+        tgt_rel = tgt_rel:sub(#notebook + 2)
+      end
+
+      local cur_dir = vim.fn.fnamemodify(cur_rel, ":h")
+      local tgt_dir = vim.fn.fnamemodify(tgt_rel, ":h")
+      local tgt_stem = vim.fn.fnamemodify(tgt_rel, ":t:r")
+      if cur_dir == "." then cur_dir = "" end
+      if tgt_dir == "." then tgt_dir = "" end
+
+      local link_path
+      if cur_dir == tgt_dir then
+        link_path = tgt_stem
+      else
+        local from = cur_dir ~= "" and vim.split(cur_dir, "/") or {}
+        local to = tgt_dir ~= "" and vim.split(tgt_dir, "/") or {}
+        local common = 0
+        for i = 1, math.min(#from, #to) do
+          if from[i] == to[i] then common = i else break end
+        end
+        local parts = {}
+        for _ = 1, #from - common do parts[#parts + 1] = ".." end
+        for i = common + 1, #to do parts[#parts + 1] = to[i] end
+        parts[#parts + 1] = tgt_stem
+        link_path = table.concat(parts, "/")
+      end
+
+      local link = "[" .. title .. "](" .. link_path .. ")"
+      local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+      vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { link })
+      vim.api.nvim_win_set_cursor(0, { row, col + #link })
+    end
 
     telescope.setup({
       defaults = {
@@ -15,10 +73,12 @@ return {
           i = {
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
             ["<C-s>"] = actions.select_horizontal,
+            ["<C-y>"] = insert_note_link,
           },
           n = {
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
             ["<C-s>"] = actions.select_horizontal,
+            ["<C-y>"] = insert_note_link,
           },
         },
         previewer = true,
